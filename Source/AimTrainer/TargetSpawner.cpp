ade720c79185df1425145ac8985096e7b22bd7cb
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-10-25 18:55:44 +0200

    feat: implement target spawning and destruction handling

--- Source/AimTrainer/TargetSpawner.cpp
+++ Source/AimTrainer/TargetSpawner.cpp
@@ -2,6 +2,7 @@
 
 
 #include "TargetSpawner.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 ATargetSpawner::ATargetSpawner()
@@ -23,5 +24,32 @@ void ATargetSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+		if (CurrentTargets < MaxTargets)
+	{
+		SpawnTarget();
+                CurrentTargets++;
+	}
+
 }
 
+void ATargetSpawner::SpawnTarget()
+{
+	if (!TargetClass) return;
+
+	FVector Origin = GetActorLocation();
+
+	FVector RandomOffset = Origin + FMath::RandPointInBox(FBox::BuildAABB(Origin, SpawnAreaExtent));
+	FTransform SpawnTransform(FRotator::ZeroRotator, RandomOffset);
+
+	AActor* SpawnedTarget = GetWorld()->SpawnActor<AActor>(TargetClass, SpawnTransform);
+
+	if (SpawnedTarget)
+	{
+		SpawnedTarget->OnDestroyed.AddDynamic(this, &ATargetSpawner::OnTargetDestroyed);
+	}
+}
+
+void ATargetSpawner::OnTargetDestroyed(AActor* DestroyedActor)
+{
+	CurrentTargets = FMath::Max(0, CurrentTargets - 1);
+}
