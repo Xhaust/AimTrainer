3b325e70585b205870f671c2f350ed7102ee85cf
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-12-13 22:05:07 +0100

    feat: create and scaffold scoreboard widget

---
+++ Source/AimTrainer/UI/Scoreboard.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ListView.h"
+#include "Components/TextBlock.h"
+#include "Scoreboard.generated.h"
+
+UCLASS()
+class AIMTRAINER_API UScoreboard : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ScenarioNameText;
+
+	UPROPERTY(meta = (BindWidget))
+	UListView* ScoreListView;
+
+	void SetScenarioName(const FString& ScenarioName);
+
+private:
+	FString CurrentScenarioName;
+	
+	UPROPERTY()
+	UUserScores* UserScores;
+
+	void UpdateScoreboard();
+};
