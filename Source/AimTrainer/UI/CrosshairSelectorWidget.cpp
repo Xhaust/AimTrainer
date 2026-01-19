37bcc61963ce5a5801f8b3741e3eb8c3ee2774fc
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-01-19 22:30:45 +0100

    feat: define CrosshairSelectorWidget functions

--- Source/AimTrainer/UI/CrosshairSelectorWidget.cpp
+++ Source/AimTrainer/UI/CrosshairSelectorWidget.cpp
@@ -2,4 +2,34 @@
 
 
 #include "CrosshairSelectorWidget.h"
+#include "CrosshairItem.h"
+#include "AimTrainer/Utils/RuntimeAssetLoader.h"
 
+void UCrosshairSelectorWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	LoadCrosshairs();
+}
+
+void UCrosshairSelectorWidget::LoadCrosshairs()
+{
+	if (!CrosshairList) return;
+
+	CrosshairList->ClearListItems();
+
+	const FString Dir = FPaths::ProjectSavedDir() / TEXT("Crosshairs");
+
+	TArray<FString> Files;
+	IFileManager::Get().FindFiles(Files, *Dir, TEXT("*.png"));
+
+	for (const FString& File : Files)
+	{
+		UCrosshairItem* Item = NewObject<UCrosshairItem>(this);
+
+		Item->FileName = File;
+		Item->FullPath = Dir / File;
+		Item->PreviewTexture = URuntimeAssetLoader::LoadTextureFromFile(Item->FullPath);
+
+		CrosshairList->AddItem(Item);
+	}
+}
