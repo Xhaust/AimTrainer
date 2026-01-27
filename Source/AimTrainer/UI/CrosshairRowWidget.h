7035efebb7a7b60ea8d58440ac4aadd2ba9b260e
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-01-27 22:55:42 +0100

    fix: make CrosshairRowWidget selectable as List Entry

--- Source/AimTrainer/UI/CrosshairRowWidget.h
+++ Source/AimTrainer/UI/CrosshairRowWidget.h
@@ -3,12 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/IUserObjectListEntry.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/Image.h"
 #include "CrosshairRowWidget.generated.h"
 
 UCLASS()
-class AIMTRAINER_API UCrosshairRowWidget : public UUserWidget
+class AIMTRAINER_API UCrosshairRowWidget : public UUserWidget, public IUserObjectListEntry
 {
 	GENERATED_BODY()
 
