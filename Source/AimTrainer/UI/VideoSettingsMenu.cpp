eb4cd00e1e8f66b55a7d6ea7cf0e0c16249a04d6
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-03-03 23:35:13 +0100

    feat: define ApplyButton logic in VideoSettings menu

--- Source/AimTrainer/UI/VideoSettingsMenu.cpp
+++ Source/AimTrainer/UI/VideoSettingsMenu.cpp
@@ -1,5 +1,6 @@
-
 #include "VideoSettingsMenu.h"
+
+#include "AimTrainer/Settings/AimTrainerUserSettings.h"
 
 void UVideoSettingsMenu::NativeOnInitialized()
 {
@@ -19,4 +20,40 @@ void UVideoSettingsMenu::NativeOnInitialized()
 	QualityCombo->AddOption("Epic");
 
 	ApplyButton->OnClicked.AddDynamic(this, &ThisClass::OnApplyClicked);
+}
+
+void UVideoSettingsMenu::OnApplyClicked()
+{
+	UAimTrainerUserSettings* Save = UAimTrainerUserSettings::GetAimTrainerUserSettings();
+	UGameUserSettings* GS = GEngine->GetGameUserSettings();
+	if (!Save || !GS) return;
+
+	FString Res = ResolutionCombo->GetSelectedOption();
+	FString XStr, YStr;
+	if (Res.Split("x", &XStr, &YStr))
+	{
+		int32 X = FCString::Atoi(*XStr);
+		int32 Y = FCString::Atoi(*YStr);
+		GS->SetScreenResolution(FIntPoint(X, Y));
+		Save->Resolution = FIntPoint(X, Y);
+	}
+
+	const FString Mode = WindowModeCombo->GetSelectedOption();
+	EWindowMode::Type WindowMode =
+		Mode == "Windowed" ? EWindowMode::Windowed :
+		Mode == "Borderless" ? EWindowMode::WindowedFullscreen :
+		EWindowMode::Fullscreen;
+
+	GS->SetFullscreenMode(WindowMode);
+	Save->WindowMode = WindowMode;
+
+	const int32 Quality = QualityCombo->GetSelectedIndex();
+	GS->SetOverallScalabilityLevel(Quality);
+	Save->OverallQuality = Quality;
+
+	GS->SetVSyncEnabled(VSyncCheckBox->IsChecked());
+	Save->bVSync = VSyncCheckBox->IsChecked();
+
+	GS->ApplySettings(false);
+	Save->SaveSettings();
 }
